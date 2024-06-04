d0dbf55bbba0fcd189291688925aa0101a3a53c1
kvhaws <khaws@realtra.space>
2024-06-04 14:29:58 +0100

    Update test_file.c
    Comments

--- test_file.c
+++ test_file.c
@@ -1,16 +1,18 @@
+// Includes
 #include <stdarg.h>
 #include <stddef.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <setjmp.h>
 #include <cmocka.h>
-
 #include "file.c"
 
+// Dummy wrapMe function to snuff out linking errors
 int32_t wrapMe(const int32_t *num) {
     return 0;
 }
 
+// Wrapped function of above wrapMe. Returns the mock type, checks if the function was fed the correct argument.
 int32_t __wrap_wrapMe(const int32_t *num)
 {
    check_expected_ptr(num);
@@ -18,6 +20,7 @@ int32_t __wrap_wrapMe(const int32_t *num)
    return mock_type(int32_t);
 }
 
+// Tells the wrapped function to expect any parameter, assigns it to return 36, then checks that using an assertion.
 static void test_wrap(void **state)
 {
     int num = 0; 
@@ -26,6 +29,7 @@ static void test_wrap(void **state)
     assert_int_equal(__wrap_wrapMe(&num), 36);
 }
 
+// Runs the test_wrap function.
 int main(void)
 {
     const struct CMUnitTest tests[] = {
