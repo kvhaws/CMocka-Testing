c5c5358ed1b03d7633ea796646046b50be0dc023
kvhaws <khaws@realtra.space>
2024-06-04 14:25:11 +0100

    Update file.c
    Comments

--- file.c
+++ file.c
@@ -1,13 +1,15 @@
+// Includes
 #include <stdarg.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <stdbool.h>
 #include <setjmp.h>
-
 #include <cmocka.h>
 
+// Dummy wrapMe function to snuff out linking errors
 int32_t wrapMe(const int *num);
 
+// sendCommand function as called through test_file.c
 bool sendCommand(int32_t num) {
     bool function_return = false;
     if (wrapMe(&num) == num)
