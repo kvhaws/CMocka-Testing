b93c6d1e0cc97284e45c52fd94c5518b52a39a36
kvhaws <khaws@realtra.space>
2024-06-04 09:40:22 +0100

    Update test_example.c
    Added a test for will_return function to begin mocking.

--- test_example.c
+++ test_example.c
@@ -29,12 +29,32 @@ static void test_example_range(void **state) {
     assert_in_range(2, 1, 7);
 }
 
+int func()
+{
+    int r = return_int();
+
+    return r;
+}
+
+int return_int(void)
+{
+    return (int)mock();
+}
+
+static void test_return(void **state) {
+    (void) state; 
+    will_return(return_int, 42);
+
+    assert_int_equal(func(), 42);
+}
+
 int main(void) {
     const struct CMUnitTest tests[] = {
         cmocka_unit_test(test_example_true),
         cmocka_unit_test(test_example_false),
         cmocka_unit_test(test_example_bool),
-        cmocka_unit_test(test_example_range)
+        cmocka_unit_test(test_example_range),
+        cmocka_unit_test(test_return)
     };
 
     return cmocka_run_group_tests(tests, NULL, NULL);
