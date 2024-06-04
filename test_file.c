d46aa371a210b7c8b1ef7fdd9b3789f60ac79e7c
kvhaws <khaws@realtra.space>
2024-06-04 14:20:30 +0100

    Update test_file.c
    Testing wrapped functions found in a different file.

--- test_file.c
+++ test_file.c
@@ -1,17 +1,35 @@
 #include <stdarg.h>
 #include <stddef.h>
+#include <stdbool.h>
 #include <stdint.h>
 #include <setjmp.h>
 #include <cmocka.h>
 
-static void test_file_example(void **state) {
-    (void) state; 
-    assert_int_not_equal(1, 7);
+#include "file.c"
+
+int32_t wrapMe(const int32_t *num) {
+    return 0;
 }
 
-int main(void) {
+int32_t __wrap_wrapMe(const int32_t *num)
+{
+   check_expected_ptr(num);
+
+   return mock_type(int32_t);
+}
+
+static void test_wrap(void **state)
+{
+    int num = 0; 
+    expect_any(__wrap_wrapMe, num);
+    will_return(__wrap_wrapMe, 36);
+    assert_int_equal(__wrap_wrapMe(&num), 36);
+}
+
+int main(void)
+{
     const struct CMUnitTest tests[] = {
-        cmocka_unit_test(test_file_example)
+        cmocka_unit_test(test_wrap)
     };
 
     return cmocka_run_group_tests(tests, NULL, NULL);
