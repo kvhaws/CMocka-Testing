77905769f8911767d711d507b527d9ec4f0f09bd
kvhaws <khaws@realtra.space>
2024-06-04 14:28:00 +0100

    Update test_example.c
    Comments

--- test_example.c
+++ test_example.c
@@ -1,34 +1,40 @@
+// Includes
 #include <stdarg.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <setjmp.h>
 #include <cmocka.h>
 
+// Declarations
 int32_t num = 16;
 int32_t num2 = 16;
 
 
-// Example test function
+// Example test function -- checks if two numbers are equal from their declarations
 static void test_example_true(void **state) {
     (void) state; // unused
     assert_int_equal(num, num2);
 }
 
+// Example test function -- checks if two numbers are equal as given
 static void test_example_false(void **state) {
     (void) state; // unused
     assert_int_equal(2, 2);
 }
 
+// Example test function -- asserts that the two given numbers are indeed not equal, rendering the statement false
 static void test_example_bool(void **state) {
     (void) state; // unused
     assert_false(5 == 6);
 }
 
+// Example test function -- checks if the first number is in the range given by the second and third integers
 static void test_example_range(void **state) {
     (void) state; 
     assert_in_range(2, 1, 7);
 }
 
+// Function to test the assertion in test_return, taking in the mocked value
 int func()
 {
     int r = return_int();
@@ -36,11 +42,13 @@ int func()
     return r;
 }
 
+// Returns the mocked value as given by will_return
 int return_int(void)
 {
     return (int)mock();
 }
 
+// Function to test the will_return and following assertion in test_return
 static void test_return(void **state) {
     (void) state; 
     will_return(return_int, 42);
@@ -48,6 +56,7 @@ static void test_return(void **state) {
     assert_int_equal(func(), 42);
 }
 
+// Runs each test
 int main(void) {
     const struct CMUnitTest tests[] = {
         cmocka_unit_test(test_example_true),
